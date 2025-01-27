135af66f00cc76070b9cd11e2e6f5a08f9e58c04
Anand Kumar Gautam <anandvns0510@gmail.com>
2025-01-28 01:39:16 +0530

    first commit

---
+++ hello.js
@@ -0,0 +1,13 @@
+// console.log("hello guyss")
+// console.log(window)
+
+// windows and use realted availeble nhi hai
+
+const math = require("./math")
+const {add,sub} = require("./math")
+
+// console.log(math(2,6))
+
+console.log(math.add(2,6))
+console.log(add(2,6))
+console.log(sub(2,6))
