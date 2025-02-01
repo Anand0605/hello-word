5c12d4163f23e170d0ccad8610e78165b62d1ed6
Anand Kumar Gautam <anandvns0510@gmail.com>
2025-02-01 19:56:02 +0530

    file system update

---
+++ file.js
@@ -0,0 +1,33 @@
+const fs = require("fs");
+
+// Synchronous
+// fs.writeFileSync("./test.txt","hey there");
+
+// Asynchronous
+// fs.writeFile("./test.txt","hey Anand",(err)=>{});
+
+// const result = fs.readFileSync("./contact.txt","utf-8");
+// console.log(result)
+
+// fs.readFile("./contact.txt","utf-8",(err,result)=>{
+//     if(err){
+//         console.log(err)
+//     }else{
+//         console.log(result)
+//     }
+// })
+
+// fs.appendFileSync("./contact.txt",new Date().getDate().toLocaleString());
+
+// fs.appendFileSync("./contact.txt", new Date().toLocaleString() + '\n');
+
+// file copy
+// fs.cpSync("./contact.txt","./copy.txt")
+
+// file delete
+// fs.unlinkSync("./copy.txt")
+
+// console.log(fs.statSync("./contact.txt").isFile());
+
+// create folder
+fs.mkdirSync("my-docs/")
